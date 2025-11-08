1d005e3427a6ca415a36b6e64edd767d6417db81
Haden Watne <haden@hadenwatne.com>
2025-11-07 21:58:30 -0700

    Update site design

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,19 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FpHeroPanel, FpTypewriterService } from '@faepoint/fp-uilib';
+import { Router } from '@angular/router';
+import { FpButton, FpFooter, FpHeroPanel, FpTypewriterService } from '@faepoint/fp-uilib';
 import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, FpHeroPanel],
+  imports: [CommonModule, FpHeroPanel, FpButton, FpFooter],
   templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
   typedHeader: Observable<string>;
 
-  constructor(private typeWriter: FpTypewriterService) {}
+  constructor(private typeWriter: FpTypewriterService, private router: Router) {}
 
   ngOnInit(): void {
     this.typedHeader = this.typeWriter.create(['Software', 'Websites', 'Hosting', 'Consulting'], true);
+  }
+
+  navigatePortfolio() {
+    this.router.navigate(['/portfolio']);
   }
 }
