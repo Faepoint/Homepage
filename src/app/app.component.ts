b3dd82f8330d5a587761418a5167c479d90888c8
Haden Watne <haden@hadenwatne.com>
2025-12-18 22:06:39 -0700

    Update site accessibility and setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { FpFooter } from '@faepoint/fp-uilib';
+import { FpFooter, FpPopMenu } from '@faepoint/fp-uilib';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, FpFooter],
+  imports: [RouterOutlet, RouterLink, FpFooter, FpPopMenu],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
