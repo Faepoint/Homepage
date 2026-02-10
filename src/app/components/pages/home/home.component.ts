7c4938e559c5d925ac995f16a8f91b9f1049ac73
Haden Watne <haden@hadenwatne.com>
2026-02-09 22:31:09 -0700

    Update deps and add social links

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,18 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { FpButton, FpHeroPanel, FpTypewriterService } from '@faepoint/fp-uilib';
+import { Router, RouterLink } from '@angular/router';
+import { FpButton, FpHeroPanel, FpModal, FpTypewriterService } from '@faepoint/fp-uilib';
 import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, FpHeroPanel, FpButton],
+  imports: [CommonModule, FpHeroPanel, FpButton, FpModal, RouterLink],
   templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
   typedHeader: Observable<string>;
 
-  constructor(private typeWriter: FpTypewriterService, private router: Router) {}
+  constructor(
+    private typeWriter: FpTypewriterService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.typedHeader = this.typeWriter.create(['Software', 'Websites', 'Hosting', 'Consulting'], true);
